6493e5de6bfa860dba57ad2dcfd2ef18f43719a1
theone <297190869@qq.com>
2025-03-02 14:07:30 +0800

    doc: update project  readme

--- cchess_reg/README.md
+++ cchess_reg/README.md
@@ -46,13 +46,13 @@ data/
 `datasets/transforms` 为扩展数据处理，以及数据增强
 
 
-- `CChessPackInputs` 
-- `CChessRandomFlip`
-- `CChessCachedMixUp`
-- `CChessHalfFlip`
-- `RandomPerspectiveTransform`
-- `CChessMixSinglePngCls`
-- `CChessCachedCopyHalf`
+- `CChessPackInputs` 替换原生 `PackInputs`
+- `CChessRandomFlip` 替换原生 `RandomFlip` 随机翻转
+- `CChessCachedMixUp` 替换原生 `CachedMixUp` 随机混合，Deprecated, 使用 `CChessMixSinglePngCls` 替代
+- `CChessHalfFlip` 镜像
+- `RandomPerspectiveTransform` 随机透视变换
+- `CChessMixSinglePngCls` 增加棋谱的棋子，多样性、以及处理分类均衡问题
+- `CChessCachedCopyHalf` 随机替换 上半部 或者 下半部 棋谱
 
 
 ## todo
@@ -65,4 +65,29 @@ data/
 - [ ] 部署
 
 
+## evaluation
 
+增加对应的 16 分类的 评估指标，以及 90 个位置 的相关评估
+
+## Models
+
+1. 添加 Chess10_9Neck 将, feature 降维 10 * 9 * 16
+2. `CChessTableHead` 继承 `MultiLabelClsHead`
+   - 将输出一个多分类的结果，改为固定 90 个位置的分类结果
+   - 以及相关损失函数的计算逻辑修改
+  
+## structures
+
+1. `CChessDataSample` 继承 `BaseDataSample` 
+   - 增加 `pred_score` 和 `gt_score` 用于存储预测得分和真实得分
+   - 增加 `pred_label` 和 `gt_label` 用于存储预测标签和真实标签
+
+
+## Script
+
+
+```bash
+# 训练
+python train.py configs/swinv2-nano_cchess16-256.py
+
+```
--- cchess_reg/datasets/transforms/cchess_formatting.py
+++ cchess_reg/datasets/transforms/cchess_formatting.py
@@ -4,30 +4,51 @@ from mmpretrain.registry import TRANSFORMS
 
 @TRANSFORMS.register_module()
 class CChessPackInputs(PackInputs):
+    """CChess 版 PackInputs 类
+    
+    继承自 mmpretrain 的 PackInputs 类，用于将预处理后的数据打包成模型所需的格式。
+    主要功能是将图像数据和标签数据打包到 CChessDataSample 结构中，以便模型训练和推理。
+    
+    属性:
+        input_key (str): 输入数据的键名，默认为 'img'
+        algorithm_keys (list): 算法相关的键列表
+        meta_keys (list): 元信息相关的键列表
+    """
+    
     def transform(self, results: dict) -> dict:
-        """Method to pack the input data."""
+        """将输入数据打包成模型所需的格式。
+        
+        Args:
+            results (dict): 包含预处理后数据的字典
+            
+        Returns:
+            dict: 打包后的数据，包含 'inputs' 和 'data_samples' 两个键
+                - inputs: 模型的输入数据，通常是图像张量
+                - data_samples: CChessDataSample 实例，包含标签、掩码等信息
+        """
 
         packed_results = dict()
         if self.input_key in results:
             input_ = results[self.input_key]
             packed_results['inputs'] = self.format_input(input_)
 
+        # 创建中国象棋数据样本实例
         data_sample = CChessDataSample()
 
-        # Set default keys
+        # 设置基础标签信息
         if 'gt_label' in results:
-            data_sample.set_gt_label(results['gt_label'])
+            data_sample.set_gt_label(results['gt_label'])  # 设置真实标签
         if 'gt_score' in results:
-            data_sample.set_gt_score(results['gt_score'])
+            data_sample.set_gt_score(results['gt_score'])  # 设置真实分数
         if 'mask' in results:
-            data_sample.set_mask(results['mask'])
+            data_sample.set_mask(results['mask'])  # 设置掩码信息
 
-        # Set custom algorithm keys
+        # 设置自定义算法所需的额外信息
         for key in self.algorithm_keys:
             if key in results:
                 data_sample.set_field(results[key], key)
 
-        # Set meta keys
+        # 设置元信息，如图像路径、尺寸等
         for key in self.meta_keys:
             if key in results:
                 data_sample.set_field(results[key], key, field_type='metainfo')
