05a4db1c65d9f3e288035c7654b77a528d708fc9
theone <297190869@qq.com>
2025-03-03 23:16:25 +0800

    feat: output onnx support

--- cchess_reg/deploy/classification_model.py
+++ cchess_reg/deploy/classification_model.py
@@ -1,77 +1,15 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from typing import Any, List, Optional, Sequence, Union
 
-import numpy as np
 import torch.nn.functional as F
 import torch
-from mmengine import Config
-from mmengine.model import BaseDataPreprocessor
-from mmengine.registry import Registry
 from mmengine.structures import BaseDataElement
-from torch import nn
-
-from mmdeploy.codebase.base import BaseBackendModel
 from mmdeploy.utils import (Backend, get_root_logger)
-from mmdeploy.codebase.mmpretrain.deploy.classification_model import __BACKEND_MODEL
-
-# __BACKEND_MODEL = Registry('backend_classifiers')
+from mmdeploy.codebase.mmpretrain.deploy.classification_model import __BACKEND_MODEL, End2EndModel
 
 
 @__BACKEND_MODEL.register_module('end2end', force=True)
-class End2EndModel(BaseBackendModel):
-    """End to end model for inference of classification.
-
-    Args:
-        backend (Backend): The backend enum, specifying backend type.
-        backend_files (Sequence[str]): Paths to all required backend files(e.g.
-            '.onnx' for ONNX Runtime, '.param' and '.bin' for ncnn).
-        device (str): A string represents device type.
-        deploy_cfg (str | Config): Deployment config file or loaded Config
-            object.
-        data_preprocessor (BaseDataPreprocessor): The data preprocessor
-                of the model. Default to `None`.
-    """
-
-    def __init__(self,
-                 backend: Backend,
-                 backend_files: Sequence[str],
-                 device: str,
-                 model_cfg: Union[str, Config] = None,
-                 deploy_cfg: Union[str, Config] = None,
-                 data_preprocessor: Optional[Union[dict, nn.Module]] = None,
-                 **kwargs):
-        super(End2EndModel, self).__init__(
-            deploy_cfg=deploy_cfg, data_preprocessor=data_preprocessor)
-        self.deploy_cfg = deploy_cfg
-        self._init_wrapper(
-            backend=backend,
-            backend_files=backend_files,
-            device=device,
-            **kwargs)
-        self.model_cfg = model_cfg
-        self.head = None
-        if model_cfg is not None:
-            self.head = self._get_head()
-        self.device = device
-
-    def _get_head(self):
-        from mmpretrain.models import build_head
-        head_config = self.model_cfg['model']['head']
-        head = build_head(head_config)
-        return head
-
-    def _init_wrapper(self, backend: Backend, backend_files: Sequence[str],
-                      device: str, **kwargs):
-        output_names = self.output_names
-        self.wrapper = BaseBackendModel._build_wrapper(
-            backend=backend,
-            backend_files=backend_files,
-            device=device,
-            input_names=[self.input_name],
-            output_names=output_names,
-            deploy_cfg=self.deploy_cfg,
-            **kwargs)
-
+class End2EndModelExt(End2EndModel):
     def forward(self,
                 inputs: torch.Tensor,
                 data_samples: Optional[List[BaseDataElement]] = None,
@@ -117,11 +55,10 @@ class End2EndModel(BaseBackendModel):
                 data_samples = [DataSample() for _ in range(cls_score.size(0))]
             
             # fix to support cchess header
-            pred_scores = F.softmax(cls_score, dim=-1)
-            pred_labels = pred_scores.argmax(dim=-1, keepdim=True).detach()
+            pred_labels = cls_score.argmax(dim=-1, keepdim=True).detach()
             pred_labels = pred_labels.squeeze(-1)
 
-            for data_sample, score, label in zip(data_samples, pred_scores, pred_labels):
+            for data_sample, score, label in zip(data_samples, cls_score, pred_labels):
                 data_sample.set_pred_score(score).set_pred_label(label)
 
         return data_samples
